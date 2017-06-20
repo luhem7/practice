4dc9f74a856ae314e693c053bcf465ffbca2b2a5
Mehul Gangavelli <mehul.gang@gmail.com>
2017-06-20 05:58:00 -0400

    Crossed Chapter 4

--- rust/rust-book/hello_strings/src/main.rs
+++ rust/rust-book/hello_strings/src/main.rs
@@ -1,5 +1,13 @@
+fn greet(name_str: &str) -> String{
+    let mut greeting = String::from("Salutations ");
+    greeting.push_str(name_str);
+    return greeting;
+}
+
 fn main() {
     let str1 = "Hello, wurld!"; 
-    let str1 = "Jello world1";
     println!("{}", str1);
+
+    let name : &str= "Bob";
+    println!("{}", greet(name));
 }
